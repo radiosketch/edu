21a313037552062d392e18de0a1680ad0403564d
radiosketch <officialradiosketch@gmail.com>
2022-07-25 19:43:50 -0400

    Included related info
    Don't lose your place

--- Software Engineering/GUI Design/GIF Editor/dev_additions.py
+++ Software Engineering/GUI Design/GIF Editor/dev_additions.py
@@ -6,6 +6,7 @@ class GIFLabel(Label):
 	"""
 	A Label that displays images, and plays them if they are gifs
 	:im: A PIL Image instance or a string filename
+	https://www.reddit.com/r/Tkinter/comments/kler90/how_can_i_use_a_gif_as_a_background_for_a/
 	"""
 	def load(self, im):
 		if isinstance(im, str):
