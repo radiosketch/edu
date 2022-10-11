7227cd7c1bc6c6915a0c3a4f27802643d2fe4464
radiosketch <officialradiosketch@gmail.com>
2022-10-11 19:57:25 -0400

    Added Rust as a section to learn more about

---
+++ Software Engineering/Rust/gif_editor/target/debug/deps/gif_editor.d
@@ -0,0 +1,5 @@
+C:\Users\rad1osketxh\edu\Software Engineering\Rust\gif_editor\target\debug\deps\gif_editor.exe: src\main.rs
+
+C:\Users\rad1osketxh\edu\Software Engineering\Rust\gif_editor\target\debug\deps\gif_editor.d: src\main.rs
+
+src\main.rs:
---
+++ Software Engineering/Rust/gif_editor/target/debug/gif_editor.d
@@ -0,0 +1,1 @@
+C:\Users\rad1osketxh\edu\Software\ Engineering\Rust\gif_editor\target\debug\gif_editor.exe: C:\Users\rad1osketxh\edu\Software\ Engineering\Rust\gif_editor\src\main.rs
