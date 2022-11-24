86023df07760cb45e65eec9a31052951b3ab309b
radiosketch <officialradiosketch@gmail.com>
2022-11-24 14:47:52 -0500

    Implemented Undo, Redo, & Cancel

--- Software Engineering/Websites/google_sheets_site/app.js
+++ Software Engineering/Websites/google_sheets_site/app.js
@@ -1,24 +1,39 @@
+/** 
+ * ===================================
+ * 
+ * Global Variables
+ * 
+ * ===================================
+ */
+
+var STARTINGID = 0;
+var CANVAS = null;
+var CTX = null;
+var DRAWHANDLER = null;
+var SNAPTOGRID = true;
+var SNAPTOLINES = true;
+var UNDOHISTORY = [];
+
 /**
- * ==============================
+ * ===================================
  * 
  * Class Definitions
  * 
- * ==============================
+ * ===================================
  */
 
 class Point {
-    constructor(ctx, pos, size=3, color='#000000') {
+    constructor(pos, size=3, color='#000000') {
         /**
-         * @param {CanvasRenderingContext2D} ctx
          * @param {Object} pos Contains values for x and y
          */
-        this.ctx = ctx;
-
+        this.ctx = CTX;
         this.x = pos.x;
         this.y = pos.y;
-
         this.size = size;
         this.color = color;
+        this.id = STARTINGID;
+        STARTINGID++;
     }
 
     draw() {
@@ -33,26 +48,29 @@ class Point {
 }
 
 class Line {
-    constructor(ctx, p1, p2, size=3, color='#000000') {
+    constructor(p1, p2, size=3, color='#000000', showPoints=true) {
         /**
-         * @param ctx
+         * A line segment, with a point on either end
          * @param {Point} p1
          * @param {Point} p2
          */
-        this.ctx = ctx;
-
+        this.ctx = CTX;
         this.p1 = p1;
         this.p2 = p2;
-
+        this.showPoints = showPoints;
         this.size = size;
         this.color = color;
-
         this.connected = false;
-
         this.length = null;
+        this.id = STARTINGID;
+        STARTINGID++;
     }
 
     draw() {
+        if (this.showPoints) {
+            this.p1.draw();
+            this.p2.draw();
+        }
         this.ctx.lineWidth = this.size;
         this.ctx.strokeStyle = this.color;
         this.ctx.beginPath();
@@ -63,54 +81,45 @@ class Line {
 }
 
 class Grid {
-    constructor(ctx, canvas, size) {
-        this.ctx = ctx;
+    constructor(canvas, size, showLines=true, showPoints=true) {
+        this.ctx = CTX;
         this.canvas = canvas;
         this.size = size;
-
         this.points = [];
+        // Initialize Points for a 2D Grid
         for (var i = 0; i < this.canvas.height; i += size) {
             for (var j = 0; j < this.canvas.width; j += size) {
                 this.points.push(new Point(
-                    this.ctx,
-                    {x: j, y: i},
-                    1,
-                    '#ffffff'
+                    {x: j, y: i}, // Basic x & y object
+                    1, '#ffffff'  // Optional Params
                 ));
             }
         }
-
         this.lines = [];
-        // Horizontal Lines
+        // Initialize Horizontal Lines
         for (var i = 0; i < this.canvas.height; i += size) {
             this.lines.push(new Line(
-                this.ctx,
-                {x: 0, y: i},                 // p1
-                {x: this.canvas.width, y: i}, // p2
-                1,
-                '#ffffff'
+                new Point({x: 0, y: i}),                 // p1
+                new Point({x: this.canvas.width, y: i}), // p2
+                1, '#ffffff', false                      // Optional Params
             ));
         }
         // Vertical Lines
         for (var i = 0; i < this.canvas.width; i += size) {
             this.lines.push(new Line(
-                this.ctx,
-                {x: i, y: 0},                  // p1
-                {x: i, y: this.canvas.height}, // p2
-                1,
-                '#ffffff'
+                new Point({x: i, y: 0}),                  // p1
+                new Point({x: i, y: this.canvas.height}), // p2
+                1, '#ffffff', false                       // Optional Params
             ));
         }
-
-        this.showPoints = true;
-        this.showLines = true;
+        this.showPoints = showPoints;
+        this.showLines = showLines;
     }
 
     draw() {
         if (this.showPoints) {
             this.points.forEach(point => point.draw());
         }
-
         if (this.showLines) {
             this.lines.forEach(line => line.draw());
         }
@@ -118,52 +127,91 @@ class Grid {
 }
 
 class DrawHandler {
-    constructor(ctx) {
-        this.ctx = ctx;
+    constructor() {
+        this.ctx = CTX;
+        // elements list is changed on undo and redo
         this.elements = [];
-        this.startingId = 0;
+        // persistent elements should not be referenced during an undo and redo
+        this.persistentElements = [];
     }
 
-    add(element) {
-        this.elements.push({shape: element, id: this.startingId});
-        this.startingId += 1;
+    pushPersistent(element) {
+        this.persistentElements.push(element);
+        this.update();
     }
 
-    remove(elementId) {
-        this.elements = this.elements.filter(element => element.id != elementId);
+    push(element) {
+        this.elements.push(element);
+        this.update();
+    }
+
+    pop() {
+        var removedElement = this.elements.pop();
+        this.update();
+        return removedElement
     }
 
     update() {
-        CTX.clearRect(0, 0, CTX.canvas.width, CTX.canvas.height);
-        this.elements.forEach(element => element.shape.draw());
+        CTX.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.persistentElements.forEach(element => element.draw());
+        this.elements.forEach(element => element.draw());
     }
 }
 
 /**
- * ==============================
+ * ===================================
  * 
- * Function Definitions
+ * Setup
  * 
- * ==============================
+ * ===================================
  */
 
-// Global variables
-var CANVAS = null;
-var CTX = null;
-var DRAWHANDLER = null;
-
-// Setup
 window.onload = function () {
     CANVAS = document.getElementById('canvas');
     CTX = CANVAS.getContext('2d');
 
-    CANVAS.addEventListener('click', createPoint);
+    // Initial EventListeners
+    CANVAS.addEventListener('click', startLine);
+    document.onkeydown = keyPress;
 
-    DRAWHANDLER = new DrawHandler(CTX);
-    var grid = new Grid(CTX, CANVAS, 50);
-    grid.showPoints = false;
-    DRAWHANDLER.add(grid);
-    DRAWHANDLER.update();
+    // The DRAWHANDLER refreshes the screen when a new element is added
+    // DRAWHANDLER also stores a list of existing elements
+    DRAWHANDLER = new DrawHandler();
+    var grid = new Grid(CANVAS, 50);
+    DRAWHANDLER.pushPersistent(grid);
+}
+
+/**
+ * ===================================
+ * 
+ * Function Definitions
+ * 
+ * ===================================
+ */
+
+function keyPress(evt) {
+    var evtobj = window.event ? event : evt;
+    if (evtobj.keyCode == 90 && evtobj.ctrlKey) { // Ctrl+z
+        undo();
+    }
+    if (evtobj.keyCode == 89 && evtobj.ctrlKey) { // Ctrl+y
+        redo();
+    }
+    if (evtobj.key == 'Escape') { // Escape
+        cancelLine();
+    }
+}
+
+function undo() {
+    var removedElement = DRAWHANDLER.pop();
+    if (!removedElement) return;
+    UNDOHISTORY.push(removedElement);
+}
+
+function redo() {
+    var newElement = UNDOHISTORY.pop();
+    if (!newElement) return;
+    DRAWHANDLER.push(newElement);
 }
 
 function getMousePos(canvas, evt) {
@@ -175,48 +223,46 @@ function getMousePos(canvas, evt) {
     };
 }
 
-function createPoint(evt) {
+function startLine(evt) {
     // TODO Snap to the nearest point of an existing line; if within a threshold
     // TODO Snap to the nearest point on a grid, if grid snapping is enabled
-    var pos = getMousePos(canvas, evt);
-    var point = new Point(CTX, pos);
-    var line = new Line(CTX, point, pos, 8);
-    if (!line.completed) {
-        CANVAS.removeEventListener('click', createPoint);
-        CANVAS.addEventListener('mousemove', adjustLine);
-        CANVAS.addEventListener('click', finishAdjustingLine);
-        CANVAS.adjustingLine = line;
-    }
-    point.draw();
+    var pos = getMousePos(CANVAS, evt);
+    var line = new Line(new Point(pos), new Point(pos), 8);
+    CANVAS.adjustingLine = line;
+    DRAWHANDLER.push(line);
+    UNDOHISTORY = [];
 
-    DRAWHANDLER.add(point);
-    DRAWHANDLER.add(line);
-    console.log('Started a Line');
+    CANVAS.removeEventListener('click', startLine);
+    CANVAS.addEventListener('mousemove', adjustLine);
+    CANVAS.addEventListener('click', finishAdjustingLine);
+}
+
+function cancelLine(evt) {
+    if (!CANVAS.adjustingLine.completed) {
+        DRAWHANDLER.pop();
+        CANVAS.removeEventListener('mousemove', adjustLine);
+        CANVAS.removeEventListener('click', finishAdjustingLine);
+        CANVAS.addEventListener('click', startLine);
+    }
 }
 
 function adjustLine(evt) {
-    var pos = getMousePos(canvas, evt);
-    CANVAS.adjustingLine.p2 = pos;
+    var pos = getMousePos(CANVAS, evt);
+    CANVAS.adjustingLine.p2.x = pos.x;
+    CANVAS.adjustingLine.p2.y = pos.y;
     DRAWHANDLER.update();
 }
 
 function finishAdjustingLine(evt) {
+    var pos = getMousePos(CANVAS, evt);
     // TODO Snap to the nearest point of an existing line; if within a threshold
     // TODO Snap to the nearest point on a grid, if grid snapping is enabled
-    var pos = getMousePos(canvas, evt);
-
     CANVAS.adjustingLine.completed = true;
-    CANVAS.removeEventListener('mousemove', adjustLine);
-    CANVAS.removeEventListener('click', finishAdjustingLine);
-    CANVAS.addEventListener('click', createPoint);
-
-    // Add a point at the end of the line
-    var point = new Point(CTX, pos);
-    DRAWHANDLER.add(point);
-
     // TODO Add a textbox to the center of the line for length
     // Attach this textbox to edit the CANVAS.adjustingLine.length
-
     DRAWHANDLER.update();
-    console.log('Finished a Line');
+
+    CANVAS.removeEventListener('mousemove', adjustLine);
+    CANVAS.removeEventListener('click', finishAdjustingLine);
+    CANVAS.addEventListener('click', startLine);
 }
